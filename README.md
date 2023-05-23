24df79ff58d65f12ab527da3d975b52e01ad9316
tiagonevestec <tiagonevestec@gmail.com>
2023-05-23 00:33:01 -0300

    Add Página 'Pokemon Detalhes', Efeito 'Pointer' e 'Hover' nos botões e links, 'Background' personalizado e 'Paragrafo' na Home.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Trilha JS Developer - Pokedex
+- Projeto desenvolvido em HTML, CSS e JS;
+- Com Consumo de API;
+- Base no FORK desafio DIO;
+- Personalização by Tiago S F Neves;
+<hr>
+
+## Patrocinado
+- Ifood
+- Digital Inovation One (DIO)
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,18 +1,27 @@
-
 const pokeApi = {}
 
-function convertPokeApiDetailToPokemon(pokeDetail) {
+function convertPokemonApiDetailToPokemon(pokeDetail){
     const pokemon = new Pokemon()
     pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
 
-    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
-    const [type] = types
+    pokemon.weight = pokeDetail.weight
+    pokemon.height = pokeDetail.height
 
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const[type] = types
     pokemon.types = types
     pokemon.type = type
 
-    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+    const abilities = pokeDetail.abilities.map((abilitySlot) => abilitySlot.ability.name)
+    const[ability] = abilities
+    pokemon.abilities = abilities
+    pokemon.ability = ability
+
+    const stats = pokeDetail.stats.map((baseStat) => (baseStat.stat.name + ' | ' + baseStat.base_stat))
+    pokemon.stats = stats
+
+    pokemon.photo = pokeDetail.sprites.other["official-artwork"].front_default
 
     return pokemon
 }
@@ -20,16 +29,24 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
         .then((response) => response.json())
-        .then(convertPokeApiDetailToPokemon)
+        .then(convertPokemonApiDetailToPokemon)
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
+pokeApi.getPokemons = (offset, limit) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}}&limit=${limit}`;
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)
+        .catch((error) => console.error(error))
+}
+
+pokeApi.getPokemonInfo = (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    return fetch(url)
+        .then((response) => response.json())
+        .then(convertPokemonApiDetailToPokemon)
+        .catch((error) => console.error(error))
 }
