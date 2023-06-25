dcd37511c37ab1376e85d1dde35647b446e5b749
Cristian Vieira <cristian.evangelista@fatec.sp.gov.br>
2023-06-25 18:44:06 -0300

    Update tooltipConfigs.js

---
+++ assets/js/tooltipConfigs.js
@@ -0,0 +1,7 @@
+  // Inicializar tooltip
+  tippy('.pokemon', {
+    content: 'Texto do Tooltip, você pode altera-lo pelo script!',
+    animation: 'perspective',
+  });
+
+  // More options and info in: https://atomiks.github.io/tippyjs/v6/getting-started/
