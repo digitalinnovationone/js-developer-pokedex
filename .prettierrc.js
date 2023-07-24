d6602dc72a64e3907e0edb066035dc9cef322334
Rafael Schenkel <rafaschenkel.dev@gmail.com>
2023-07-24 12:19:04 -0300

    Adicionado exibição de detalhes do Pokemon

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    printWidth: 120,
+    trailingComma: 'none',
+    semi: true,
+    singleQuote: true,
+    endOfLine: 'auto',
+    bracketSpacing: true,
+    bracketSameLine: false,
+    jsxBracketSameLine: false,
+    arrowParens: 'avoid',
+    singleAttributePerLine: true
+};
--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -1,8 +1,9 @@
-
 class Pokemon {
-    number;
-    name;
-    type;
-    types = [];
-    photo;
+    constructor(id, name, types, img) {
+        this.id = id;
+        this.name = name;
+        this.types = types.map(typeSlot => typeSlot.type.name[0].toUpperCase() + typeSlot.type.name.substring(1));
+        this.type = this.types[0];
+        this.img = img;
+    }
 }
