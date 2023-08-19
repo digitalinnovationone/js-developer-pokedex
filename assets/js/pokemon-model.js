09c4af31ebcf8db5e7e8e8748aaeec62fd450d85
Artur Lemes Moretti <artur.lmoretti@gmail.com>
2023-08-19 17:47:07 -0300

    feat: style pokedex

--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -1,8 +1,26 @@
-
+/**
+ * Pokemon model
+ * @param {number} number
+ * @param {string} name
+ * @param {string} type
+ * @param {string[]} types
+ * @param {string} photo
+ * @param {object[]} stats
+ */
 class Pokemon {
-    number;
-    name;
-    type;
-    types = [];
-    photo;
+  number;
+  name;
+  type;
+  types = [];
+  photo;
+  stats = [];
+
+  constructor(number, name, type, types, photo ,stats) {
+    this.number = number;
+    this.name = name;
+    this.type = type;
+    this.types = types;
+    this.photo = photo;
+    this.stats = stats;
+  }
 }
