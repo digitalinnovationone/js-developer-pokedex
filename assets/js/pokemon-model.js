6a022c988138c8cbaac7c00b0c25292950a9022d
EwertonHecsley <114318366+EwertonHecsley@users.noreply.github.com>
2023-08-19 15:53:00 -0300

    Update pokemon-model.js

--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -1,8 +1,16 @@
-
 class Pokemon {
+
     number;
     name;
-    type;
+    mainType;
     types = [];
-    photo;
+    image;
+
+    constructor(number, name, types, mainType, image) {
+        this.number = number;
+        this.name = name;
+        this.types = types;
+        this.mainType = mainType;
+        this.image = image;
+    }
 }
