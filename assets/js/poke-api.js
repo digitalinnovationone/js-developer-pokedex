73596ef8ab25fda15052ba854b238989bee7136f
Ramon Rodrigues <ramon.rpa2018@gmail.com>
2023-08-21 16:31:59 -0300

    Mudança no padrão de código, mudança em estilos e adição de novas funcionalidades
    Mudado padrão de codígo assíncrono para Async/Await. Adicionado sistema de scroll infinito para que não seja necessário botões para carregamento de novos pokemons. Modificado os estilos dos cards dos pokemons com gradientes e blur. Adicionado animações de hover.
    Foi adicionado um novo modal para trazer informações extras sobre os pokemons. Foi criado dois novos elementos personalizado utilizando a API do Custom Elements, sendo esses 2 elementos o "PokemonCard" e o "PokemonModal", onde facilita a reutilização destes elementos.

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,35 +1,17 @@
+const pokeApi = {};
 
-const pokeApi = {}
+pokeApi.getPokemonDetail = async (name) => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
 
-function convertPokeApiDetailToPokemon(pokeDetail) {
-    const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.id
-    pokemon.name = pokeDetail.name
+  return await response.json();
+};
 
-    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
-    const [type] = types
+pokeApi.getPokemons = async (offset = 0, limit = 5) => {
+  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-    pokemon.types = types
-    pokemon.type = type
+  const response = await fetch(url);
 
-    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+  const jsonBody = await response.json();
 
-    return pokemon
-}
-
-pokeApi.getPokemonDetail = (pokemon) => {
-    return fetch(pokemon.url)
-        .then((response) => response.json())
-        .then(convertPokeApiDetailToPokemon)
-}
-
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
-    return fetch(url)
-        .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)
-        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-        .then((detailRequests) => Promise.all(detailRequests))
-        .then((pokemonsDetails) => pokemonsDetails)
-}
+  return jsonBody.results;
+};
