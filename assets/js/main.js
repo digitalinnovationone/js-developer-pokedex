ce1261642a2a4b9f47c4d3672e537e312d106f17
Daniel Oliveira <contato@danieloliveira.eti.br>
2023-08-22 17:14:54 -0300

    feat: concluindo a modal com detalhes dos pokemons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
+const detalhesModal = document.getElementById('pokemonDetalhes')
+const conteudo = document.querySelector(".content");
 
 const maxRecords = 151
 const limit = 10
@@ -7,7 +9,7 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
+        <li class="pokemon ${pokemon.type}" id="${pokemon.number}">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
@@ -45,3 +47,64 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+//evento click na lista
+conteudo.addEventListener('click', function (e) {
+    const pokemonSelecionado = e.target.closest("li")
+    const pokemonId = pokemonSelecionado.id   
+
+        if(typeof parseInt(pokemonId) == 'number' && pokemonId !== ''){
+            getDetalhes(pokemonId);
+
+        }    
+})
+
+function getDetalhes(pokemonNumber) {
+
+    pokeApi.getPokemonModal(pokemonNumber).then((detalhes = []) => {
+        detalhesModal.innerHTML = `
+        <li class="pokemonOpen ${detalhes.type}">
+            <span class="nameOpen">${detalhes.name}</span>
+            <div id="divButtonClose">
+             #${detalhes.number}
+                <span class="botaoFechar" onclick="closeModal()" ontouch="closeModal()">&times;</span>
+            </div>
+
+            <div class="typesOpen">
+                <ol class="typesOpen"><span>Types: </span>
+                ${detalhes.types.map((type) => `<span class="typeOpen ${type}">${type}</span>`).join('')}
+                </ol>
+            </div>
+        
+            <div class="divSkills">
+                <div class="skills"> 
+                    <span>skills: </span>                
+                    <span class="skillOpen ${detalhes.type}">${detalhes.skill1}</span>
+                    <span class="skillOpen ${detalhes.type}">${detalhes.skill2}</span>
+                </div>
+            </div>
+
+            <div class="imageOpen">
+                <img class="images"src="${detalhes.photo}" alt="${detalhes.name}">
+            </div>
+
+            <div class="divAtributes">
+                <div class="atributesHeader">Atributes:</div>
+                <div class="atribute">Hp</div>
+                <div class="values">${detalhes.statValueHp}</div>
+                <div class="atribute">Attack</div>
+                <div class="values">${detalhes.statValueAtk}</div>
+                <div class="atribute">Defense</div>
+                <div class="values">${detalhes.statValueDef}</div>
+                <div class="atribute">special-atk</div>
+                <div class="values">${detalhes.statValueSatk}</div>
+                <div class="atribute">special-def</div>
+                <div class="values">${detalhes.statValueSdef}</div>
+                <div class="atribute">Speed</div>
+                <div class="values">${detalhes.statValueSpd}</div>
+
+            </div>
+        </li>
+        `
+    })
+}
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -14,6 +14,21 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
 
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
 
+    const skills = pokeDetail.abilities.map((skillsSlot) => skillsSlot.ability.name)
+    const [skill1, skill2 = 'not have 2nd skill'] = skills 
+    pokemon.skill1 = skill1  
+    pokemon.skill2 = skill2
+
+    const statsValues = pokeDetail.stats.map((statsSlot) => statsSlot.base_stat) 
+    const [statValueHp, statValueAtk, statValueDef, statValueSatk, statValueSdef, statValueSpd] = statsValues
+
+    pokemon.statValueHp = statValueHp 
+    pokemon.statValueAtk = statValueAtk
+    pokemon.statValueDef = statValueDef
+    pokemon.statValueSatk = statValueSatk
+    pokemon.statValueSdef = statValueSdef
+    pokemon.statValueSpd = statValueSpd
+
     return pokemon
 }
 
@@ -32,4 +47,15 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)
+}
+
+pokeApi.getPokemonModal = async (pokemonNumber) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemonNumber}/`
+    const response = await fetch(url);
+    const pokeDetail = await response.json();
+    const pokemonDetail = convertPokeApiDetailToPokemon(pokeDetail);
+
+    openModal();
+    
+    return pokemonDetail;
 }
