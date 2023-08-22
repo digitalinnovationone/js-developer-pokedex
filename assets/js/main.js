020bf50e2a2c445129a2857ba643e8ea0a27f30c
Pedro Lima <phlimas@outlook.com>
2023-08-22 14:32:08 -0300

    feat: Set new modal to show detail from pokeApi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,32 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const closeModalBtn = document.querySelector(".btn-close");
 
 const maxRecords = 151
 const limit = 10
 let offset = 0;
 
+
+const openModal = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
+
 function convertPokemonToLi(pokemon) {
+
     return `
         <li class="pokemon ${pokemon.type}">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
-            <div class="detail">
+            <div class="detail" onClick="showModal(${pokemon.number})">
                 <ol class="types">
                     ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
@@ -21,6 +36,49 @@ function convertPokemonToLi(pokemon) {
             </div>
         </li>
     `
+}
+
+function convertSigleDataToModal(pokeData) {
+    const { id, name, weight, height,
+        abilities, attack, defense, speed, image, type, description } = pokeData                
+        modal.classList.add(`${type}`);
+    return `
+    <div class="flex">
+        <img
+          src="${image}"
+          width="50px"
+          height="50px"
+          alt="pokemon image"
+        />
+      </div>
+      <div>
+        <button class="btn-close" onClick="closeModal()">⨉</button>
+        <div class="head-title">
+          <h3>${name}</h3>
+          <h3>#${id}</h3>
+        </div>
+        <p>
+          ${description}
+        </p>
+        <ul>
+          <li>Altura: ${height}</li>
+          <li>Peso: ${weight}</li>
+          <li>Ataque: ${attack}</li>
+          <li>Defesa: ${defense}</li>
+          <li>Velocidade: ${speed}</li>
+          <li>Abilidade: ${abilities.ability.name}</li>
+        </ul>
+      </div>
+    `
+}
+
+
+
+async function showModal(pokemonId) {
+    const description = await pokeApi.getDescription(pokemonId)
+    const modalData = await pokeApi.getSinglePokemon(pokemonId).then(res =>  convertSigleDataToModal({...res, description:description}))
+    modal.innerHTML = modalData
+    openModal()
 }
 
 function loadPokemonItens(offset, limit) {
@@ -45,3 +103,7 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+closeModalBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -17,6 +17,24 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     return pokemon
 }
 
+function convertDataToModal(pokeData) {
+    const pokemon = new PokemonStatus()
+
+    const types = pokeData.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+    pokemon.id = pokeData.id;
+    pokemon.name = pokeData.name;
+    pokemon.weight = pokeData.weight;
+    pokemon.height = pokeData.height;
+    pokemon.type = type;
+    pokemon.abilities = pokeData.abilities[0];
+    pokemon.attack = pokeData.stats[1].base_stat;
+    pokemon.defense = pokeData.stats[2].base_stat;
+    pokemon.speed = pokeData.stats[5].base_stat;
+    pokemon.image = pokeData.sprites.other.dream_world.front_default;
+    return pokemon       
+}
+
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
         .then((response) => response.json())
@@ -32,4 +50,17 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)
+}
+
+pokeApi.getSinglePokemon = (pokemonId) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`
+    return fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => convertDataToModal(jsonBody))
+    .then(res => res)
+}
+
+pokeApi.getDescription = async (order) => {
+    const url = `https://pokeapi.co/api/v2/pokemon-species/${order}/`
+    return await fetch(url).then(res => res.json()).then(data => data.flavor_text_entries[0].flavor_text)
 }
