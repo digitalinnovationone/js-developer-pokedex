c0ba6e83dcbbaba764a0c7a5167eb32d90831b10
kayo <kayohramos@gmail.com>
2023-08-23 11:32:44 -0300

    feat: add função de aparecer pokemon ao clicar e button de delimitar, style para o pokemon ao clicar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,32 @@
-const pokemonList = document.getElementById('pokemonList')
-const loadMoreButton = document.getElementById('loadMoreButton')
+const pokemonList = document.getElementById('pokemonList');
+const loadMoreButton = document.getElementById('loadMoreButton');
 
-const maxRecords = 151
-const limit = 10
+const pokemonDetailsContainer = document.querySelector('.pokemon-details');
+const detailName = document.querySelector('.detail-name');
+const detailTypeList = document.querySelector('.detail-type-list');
+const detailImage = document.querySelector('.detail-image img');
+
+let pokemons = []; // Armazena a lista de pokemons carregados
+const maxRecords = 151;
+const limit = 10;
 let offset = 0;
+
+pokemonList.addEventListener('click', event => {
+    const clickedPokemon = event.target.closest('.pokemon');
+    if (clickedPokemon) {
+        const pokemonIndex = Array.from(pokemonList.children).indexOf(clickedPokemon);
+        const selectedPokemon = pokemons[pokemonIndex];
+        showPokemonDetails(selectedPokemon);
+    }
+});
+
+function showPokemonDetails(pokemon) {
+    detailName.textContent = pokemon.name;
+    detailTypeList.innerHTML = pokemon.types.map(type => `<li class="type ${type}">${type}</li>`).join('');
+    detailImage.src = pokemon.photo;
+    pokemonDetailsContainer.classList.add('active');
+}
+
 
 function convertPokemonToLi(pokemon) {
     return `
@@ -23,25 +46,43 @@ function convertPokemonToLi(pokemon) {
     `
 }
 
-function loadPokemonItens(offset, limit) {
-    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML += newHtml
-    })
+
+function loadPokemonItems(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemonsData) => {
+        pokemons = pokemonsData;
+        const newHtml = pokemons.map(convertPokemonToLi).join('');
+        pokemonList.innerHTML = newHtml; // Substituir o conteúdo anterior
+
+        if (offset > 0) {
+            showLessButton.style.display = 'block'; // Exibe o botão quando não estiver no início
+        } else {
+            showLessButton.style.display = 'none'; // Esconde o botão quando estiver no início
+        }
+    });
 }
 
-loadPokemonItens(offset, limit)
+loadPokemonItems(offset, limit);
 
 loadMoreButton.addEventListener('click', () => {
-    offset += limit
-    const qtdRecordsWithNexPage = offset + limit
+    offset += limit;
+    const qtdRecordsWithNextPage = offset + limit;
 
-    if (qtdRecordsWithNexPage >= maxRecords) {
-        const newLimit = maxRecords - offset
-        loadPokemonItens(offset, newLimit)
-
-        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    if (qtdRecordsWithNextPage >= maxRecords) {
+        const newLimit = maxRecords - offset;
+        loadPokemonItems(offset, newLimit);
+        loadMoreButton.parentElement.removeChild(loadMoreButton);
     } else {
-        loadPokemonItens(offset, limit)
+        loadPokemonItems(offset, limit);
     }
-})
+});
+
+const showLessButton = document.getElementById('showLessButton');
+showLessButton.addEventListener('click', () => {
+    if (offset > 0) {
+        offset -= limit;
+        loadPokemonItems(offset, limit);
+        if (offset === 0) {
+            showLessButton.style.display = 'none'; // Esconde o botão quando estiver no início
+        }
+    }
+});
