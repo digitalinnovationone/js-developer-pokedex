7fc477f3cd81c6d7b8ae4ba95af4bee3de89ed75
Guilherme Faria <guifariadepaula14@gmail.com>
2023-08-24 13:57:16 -0300

    subindo alterações do desafio

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 
 const maxRecords = 151
-const limit = 10
+const limit = 10;
 let offset = 0;
 
 function convertPokemonToLi(pokemon) {
@@ -14,6 +14,8 @@ function convertPokemonToLi(pokemon) {
             <div class="detail">
                 <ol class="types">
                     ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${pokemon.level}</li>`).join('')}
+                    
                 </ol>
 
                 <img src="${pokemon.photo}"
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -14,13 +14,16 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
 
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
 
+    pokemon.level = pokeDetail.abilities[0].ability.name
     return pokemon
 }
+
 
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
         .then((response) => response.json())
         .then(convertPokeApiDetailToPokemon)
+        
 }
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
