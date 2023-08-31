836dc9dba18be6c33bb63eab9ca5d9cb4ca95653
Saulo Andrade <sauloandradegames@gmail.com>
2023-08-31 08:51:58 -0300

    feat: add pagina de detalhes do pokemon + mudanca na cor do texto para melhoria de legibilidade

--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -5,4 +5,16 @@ class Pokemon {
     type;
     types = [];
     photo;
+
+    weight;
+    height;
+
+    base_stats = {
+        hp:-1,
+        attack:-1,
+        defense:-1,
+        specialAttak:-1,
+        specialDefense:-1,
+        speed:-1
+    };
 }
