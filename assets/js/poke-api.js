a5c3db1463e06e84a2c2b1007cbb67cf0e48ece5
Francisco Fernandes Garcia <121530739+FranciscoFern@users.noreply.github.com>
2023-09-03 00:42:08 -0400

    modificações feitas

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,11 +1,12 @@
 
+
 const pokeApi = {}
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
     pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
-
+    
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
 
@@ -17,19 +18,20 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     return pokemon
 }
 
-pokeApi.getPokemonDetail = (pokemon) => {
-    return fetch(pokemon.url)
-        .then((response) => response.json())
-        .then(convertPokeApiDetailToPokemon)
+pokeApi.getPokemonDetails = (pokemon) => {
+ return fetch(pokemon.url)
+ .then((response) => response.json())
+ .then(convertPokeApiDetailToPokemon)
 }
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
     return fetch(url)
-        .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)
-        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-        .then((detailRequests) => Promise.all(detailRequests))
-        .then((pokemonsDetails) => pokemonsDetails)
+       .then((response) => response.json())
+       .then((jsonBody) => jsonBody.results)
+       .then((pokemons) => pokemons.map(pokeApi.getPokemonDetails))
+       .then((detailRequests) => Promise.all(detailRequests))
+       .then((pokemonsDetails) => pokemonsDetails)
 }
+  
