07ae097e74fe8c3fcd240dbfe5b0b71bf56b9ae7
Luis Felipe Ramalho Oliveira <50757892+luisfeliperamalho@users.noreply.github.com>
2023-09-18 19:06:00 -0300

    criacao arquivo pokemon-detail.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,19 +7,29 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
+      <a href="pokemon-detail.html">
+        <li class="pokemon ${pokemon.type}"">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
 
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+                    
+                    <img src="${pokemon.photo}"
+                        alt="${pokemon.name}">
+                </div>
+            </li>
+        </a>
+    `
+}
 
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
-            </div>
-        </li>
+function showDetailPokemon(pokemon) {
+    return ` 
+    <span class="number">#${pokemon.number}</span>
+    <span class="name">${pokemon.name}</span>
+    .
     `
 }
 
