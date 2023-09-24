09ae2ddc06c4c18f41a3deed8718ee231e5afdaa
JessieCastanheira <jessicab_castanheira@hotmail.com>
2023-09-24 12:58:03 -0300

    meu projeto

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -19,17 +19,19 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
 
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
-        .then((response) => response.json())
-        .then(convertPokeApiDetailToPokemon)
+    .then((response) => response.json())
+    .then(convertPokeApiDetailToPokemon)
 }
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
+    
     return fetch(url)
         .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)
+        .then((jasonBody) => jasonBody.results)
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-        .then((detailRequests) => Promise.all(detailRequests))
+        .then((detailRequest) => Promise.all(detailRequest))
         .then((pokemonsDetails) => pokemonsDetails)
-}
+} 
+
+ 
