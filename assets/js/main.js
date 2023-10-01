6d9e4c12f14a6024f431bcd13ff9eb2b6748e931
lucopdev <lucopdev@gmail.com>
2023-10-01 19:50:03 -0300

    feature: Criado um card de detalhes ao clicar no pokemon selecionado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
-const pokemonList = document.getElementById('pokemonList')
-const loadMoreButton = document.getElementById('loadMoreButton')
-
-const maxRecords = 151
-const limit = 10
+const pokemonList = document.getElementById('pokemonList');
+const loadMoreButton = document.getElementById('loadMoreButton');
+const modalContainer = document.getElementById('modal-container');
+const maxRecords = 151;
+const limit = 10;
 let offset = 0;
 
 function convertPokemonToLi(pokemon) {
-    return `
+  return `
         <li class="pokemon ${pokemon.type}">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
@@ -15,33 +15,79 @@ function convertPokemonToLi(pokemon) {
                 <ol class="types">
                     ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
-
+                
                 <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
+                alt="${pokemon.name}">
+                </div>
+                </li>
+                `;
+}
+
+const detailsFunction = (pokemonList) => {
+  const list = pokemonList.children;
+
+  for (let i = 0; i < list.length; i += 1) {
+    list[i].addEventListener('click', async () => {
+      pokemonName = list[i].children[1].innerHTML;
+      pokemonDetails = await getPokemonsDetailed(pokemonName);
+      modalContainer.innerHTML = `
+        <div class="modal">
+            <div class="modal-inner-container">
+                <div class="title-container">
+                    <h2>${pokemonDetails.species.name}</h2>
+                    <button onclick="modalClose()" class="close-btn">X</button>
+                </div>
+                <div class="superior ${pokemonDetails.types[0].type.name}">
+                    <img src=${
+                      pokemonDetails.sprites.other.dream_world.front_default
+                    } alt="pokemon-img"/>
+                    </div>
+                    <div class="floor">
+                    </div>
+                    <div class="inferior">
+                    <p>Details</p>
+                    ${pokemonDetails.stats.map(
+                      (stat, index) => `
+                        <div class="stats-container">
+                            <p key=${index} class="stats">${stat.stat.name}</p>
+                            <p>${stat.base_stat}</p>
+                        </div>
+                        <div class="stats-inner-container ${pokemonDetails.types[0].type.name}"></div>
+                    `
+                    )}
+                </div>
             </div>
-        </li>
-    `
+        <div/>
+        `;
+    });
+  }
+};
+
+async function loadPokemonItens(offset, limit) {
+  const pokemons = await pokeApi.getPokemons(offset, limit);
+  const newHtml = pokemons.map(convertPokemonToLi).join('');
+  pokemonList.innerHTML += newHtml;
+
+  const modal = await detailsFunction(pokemonList);
+  // modalContainer.appendChild(modal)
 }
 
-function loadPokemonItens(offset, limit) {
-    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML += newHtml
-    })
-}
-
-loadPokemonItens(offset, limit)
+const pokemons = loadPokemonItens(offset, limit);
 
 loadMoreButton.addEventListener('click', () => {
-    offset += limit
-    const qtdRecordsWithNexPage = offset + limit
+  offset += limit;
+  const qtdRecordsWithNexPage = offset + limit;
 
-    if (qtdRecordsWithNexPage >= maxRecords) {
-        const newLimit = maxRecords - offset
-        loadPokemonItens(offset, newLimit)
+  if (qtdRecordsWithNexPage >= maxRecords) {
+    const newLimit = maxRecords - offset;
+    loadPokemonItens(offset, newLimit);
 
-        loadMoreButton.parentElement.removeChild(loadMoreButton)
-    } else {
-        loadPokemonItens(offset, limit)
-    }
-})
+    loadMoreButton.parentElement.removeChild(loadMoreButton);
+  } else {
+    loadPokemonItens(offset, limit);
+  }
+});
+
+const modalClose = async () => {
+  modalContainer.lastElementChild.remove();
+};
