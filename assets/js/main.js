14b85424a933efdc56a1b7f30eea665b41297306
Bárbara Garmendia <barbaragarmendia@hotmail.com>
2023-10-14 15:55:49 +0100

    (feat) add: adicionando detalhes de pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
+    <li class="pokemon ${pokemon.type}" onClick="loadDetailsPokemon('${pokemon.name}')">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
@@ -30,6 +30,33 @@ function loadPokemonItens(offset, limit) {
     })
 }
 
+async function loadDetailsPokemon(pokeName) {
+    const pokemon = await pokeApi.getDetails(pokeName).then(details => details);
+
+    let description = document.querySelector('#description');
+    description.style.display = 'block';
+
+    description.innerHTML = `
+        <div class="pokemon-card">
+            <img src="${pokemon.image}" alt="${pokemon.name}">
+            <h1 class="pokemon-name">${pokemon.name.toUpperCase()}</h1>
+            <div class="pokemon-type"><strong>HP:</strong> ${pokemon.hp}</div>
+            <div class="pokemon-type"><strong>ATTACK:</strong> ${pokemon.attack}</div>
+            <div class="pokemon-type"><strong>DEFFENSE:</strong> ${pokemon.deffense}</div>
+            <div class="pokemon-type"><strong>SPECIAL ATTACK::</strong> ${pokemon.specialAttack}</div>
+            <div class="pokemon-type"><strong>SPECIAL DEFFENSE:</strong> ${pokemon.specialDefense}</div>
+            <div class="pokemon-type"><strong>SPEED:</strong> ${pokemon.speed}</div>
+        </div>
+    `;
+
+    let btn = document.createElement('button');
+    btn.className = "btnclose";
+    btn.innerHTML = 'Limpar';    
+    btn.addEventListener('click', ()=>{
+        description.style.display = 'none';
+    });
+    description.appendChild(btn);
+}
 loadPokemonItens(offset, limit)
 
 loadMoreButton.addEventListener('click', () => {
