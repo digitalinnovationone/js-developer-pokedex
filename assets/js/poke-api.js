f88248796ca0723bc052c538e42bd15cd331873a
georgia <georgiab543@gmail.com>
2023-10-14 16:49:24 -0300

    feat: add details dos pokemons e corrigindo identaçao do código e trocando todos os valores que estavam em pixels para rem

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -23,6 +23,26 @@ pokeApi.getPokemonDetail = (pokemon) => {
         .then(convertPokeApiDetailToPokemon)
 }
 
+pokeApi.getDetails = async (name) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${name}`
+    return await fetch(url)
+        .then((response) => response.json())
+        .then((body) => {
+            const pokemon = {
+                name: body.name,
+                hp: body.stats[0].base_stat,
+                attack: body.stats[1].base_stat,
+                deffense: body.stats[2].base_stat,
+                specialAttack: body.stats[3].base_stat,
+                specialDefense: body.stats[4].base_stat,
+                speed: body.stats[5].base_stat,
+                image: body.sprites.other.dream_world.front_default
+            }
+            return pokemon;
+        })
+        .catch((error) => console.log(error));
+}
+
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
