efd5597bf57ddce54e22c37f47d475af6caf4d51
Lucas Siqueira <lucasiqueira@Lucass-MacBook-Air.local>
2023-10-18 22:32:53 -0300

    feat: implements pokemon details view

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -33,3 +33,11 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)
 }
+
+pokeApi.getPokemon = (pokemonId) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
+}
