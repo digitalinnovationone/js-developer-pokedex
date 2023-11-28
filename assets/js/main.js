e16512fa6c6e7d8774f9b2d18e51beb0ade8ec40
Anderson Inacio <anderson.mouratoo@gmail.com>
2023-11-28 20:47:03 -0300

    save

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,8 @@ const loadMoreButton = document.getElementById('loadMoreButton')
 const maxRecords = 151
 const limit = 10
 let offset = 0;
+let olList = [];
+let currentPokemons = []
 
 function convertPokemonToLi(pokemon) {
     return `
@@ -25,8 +27,13 @@ function convertPokemonToLi(pokemon) {
 
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        currentPokemons.push(...pokemons);
         const newHtml = pokemons.map(convertPokemonToLi).join('')
         pokemonList.innerHTML += newHtml
+        olList = [...document.querySelector('#pokemonList').children]
+        olList.map((item, k) => item.addEventListener('click', ()=> 
+            showDetail( item.querySelector('span.number').innerText.slice(1) )
+        ))
     })
 }
 
@@ -45,3 +52,54 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+
+function showDetail(id){
+    const [pokeDetail] = currentPokemons.filter((pokemon) => pokemon.number == id )
+    let modal = document.querySelector('#modalShowDatail')
+    const html = createModalPokemon(pokeDetail)
+
+    //console.log(pokeDetail.types)
+
+    modal.innerHTML = html
+    const btnModal = document.querySelector("#btnModal");
+    btnModal.click();
+}
+
+function createModalPokemon(pokemon){  
+    return`
+        <div class="modal-dialog">
+            <div class="modal-content">
+
+                <div class="modal-body ${pokemon.type}">
+                    <section>
+                        <div class="modal-header">
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+            
+                        <div class="description">
+                            <h1 class="name"> ${pokemon.name} </h1>
+                            <span class="number"> <span>#${pokemon.number} </span> </span>
+            
+                            <ol class="types">
+                                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                            </ol>
+                        </div>
+            
+                        <div class="img-pokemon">
+                            <img src="${pokemon.photo}" alt="${pokemon.name}">
+                        </div>
+                        
+                    </section>
+
+                    <div class="modal-detail">
+                        <ul class="types">
+                            ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+                        </ul>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    `
+}
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -5,14 +5,16 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
     pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
-
+    
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
-
     pokemon.types = types
     pokemon.type = type
-
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+   
+    urlDetail = pokeDetail.abilities[0].ability.url
+
+    console.log(pokemon)
 
     return pokemon
 }
@@ -22,6 +24,13 @@ pokeApi.getPokemonDetail = (pokemon) => {
         .then((response) => response.json())
         .then(convertPokeApiDetailToPokemon)
 }
+
+pokeApi.getDetail = (pokemon) => {
+    return fetch(pokemon.abilities[0].ability.url)
+        .then((response) => response.json())
+        //.then(convertPokeApiDetailToPokemon)
+}
+
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
@@ -33,3 +42,12 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)
 }
+
+
+/**
+ * abilities => name
+ * abilities => url
+ * url => effect_entries => effect
+ * url => effect_entries => short_effect
+ * url => generation = > name
+ */
