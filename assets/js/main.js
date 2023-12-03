321b3e501c2207594571e87c1758e477b7959332
Hudson Kennedy <hudsonhugo@bol.com.br>
2023-12-03 13:45:36 -0300

    Subindo as alterações do projeto

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,8 @@ const loadMoreButton = document.getElementById('loadMoreButton')
 const maxRecords = 151
 const limit = 10
 let offset = 0;
+let olList = [];
+let currentPokemons = []
 
 function convertPokemonToLi(pokemon) {
     return `
@@ -25,8 +27,13 @@ function convertPokemonToLi(pokemon) {
 
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        currentPokemons.push(...pokemons);
         const newHtml = pokemons.map(convertPokemonToLi).join('')
         pokemonList.innerHTML += newHtml
+        olList = [...document.querySelector('#pokemonList').children]
+        olList.map((item, k) => item.addEventListener('click', ()=> 
+            showDetail( item.querySelector('span.number').innerText.slice(1) )
+        ))
     })
 }
 
@@ -45,3 +52,76 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+
+function showDetail(id){ 
+    const [pokeDetail] = currentPokemons.filter((pokemon) => pokemon.number == id )
+    let modal = document.querySelector('#modalShowDatail')
+    
+    fetch(pokeDetail.urlDetail)
+    .then((response) => response.json())
+    .then((response) => response)
+    .then((detail)=>{
+        const html = createModalPokemon(pokeDetail, detail)
+        modal.innerHTML = html
+        const btnModal = document.querySelector("#btnModal");
+        btnModal.click();
+    })
+    .catch((error) => console.log(error))
+}
+        
+
+    
+    
+
+function createModalPokemon(pokemon, datail){  
+    return`
+        <div class="modal-dialog">
+            <div class="modal-content">
+
+                <div class="modal-body ${pokemon.type}">
+                    <section>
+                        <div class="modal-header">
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+            
+                        <div class="description">
+                            <h1 class="name"> ${pokemon.name} </h1>
+                            <span class="number"> <span>#${pokemon.number} </span> </span>
+            
+                            <ol class="types">
+                                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                            </ol>
+                        </div>
+            
+                        <div class="img-pokemon">
+                            <img src="${pokemon.photo}" alt="${pokemon.name}">
+                        </div>
+                        
+                    </section>
+
+                    <div class="modal-detail">
+                        <div class="modal-detail-content">
+                            <h6>🪄 Efeitos </h6>
+                            <p class="textInfo text">${datail.effect_entries[0].effect} </p>
+                            <hr>
+
+                            <h6>🐱 Geração </h6>
+                            <p class="textInfo">${datail.generation.name} </p>
+                            <hr>
+
+                            <h6>🥷🏽 Experiência </h6>
+                            <p class="textInfo">XP-${pokemon.experience} </p>
+                            <hr>
+
+                            <h6>🏹 Habilidades </h6>
+                            <ul>
+                                ${pokemon.abilities.map((abilitie) => `<li class="textInfo">${abilitie}</li>`).join('')}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+}
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -5,14 +5,16 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
     pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
-
+    
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
-
     pokemon.types = types
     pokemon.type = type
-
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+   
+    pokemon.urlDetail = pokeDetail.abilities[0].ability.url
+    pokemon.experience = pokeDetail.base_experience
+    pokemon.abilities = pokeDetail.abilities.map((ability) => ability.ability.name)
 
     return pokemon
 }
