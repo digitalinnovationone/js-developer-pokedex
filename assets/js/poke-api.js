cd5784940c8cf3109f9b556790cc6fea9bd014a5
Roger <rogeryann@hotmail.com>
2023-12-12 17:34:22 -0300

    D

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,43 +1,84 @@
 // Objeto que contém métodos relacionados à PokeAPI
 const pokeApi = {}
 
-// Função para converter detalhes da PokeAPI em objetos Pokemon
-function convertPokeApiDetailToPokemon(pokeDetail) {
-    const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.id
-    pokemon.name = pokeDetail.name
-    
-    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)  // Mapeia os tipos do Pokémon
-    const [type] = types
+// Função que obtém uma lista de Pokémon da PokeAPI com base no deslocamento e limite
+pokeApi.getPokemons = function(offset, limit) {
+    // Define parâmetros padrão para offset e limit, caso não sejam fornecidos
+    offset = offset || 0;
+    limit = limit || 10;
 
-    pokemon.types = types // Configura propriedades do Pokémon
-    pokemon.type = type
+    // Constrói a URL para a requisição da lista de Pokémon
+    const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
 
-    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default // Obtém a URL da imagem do Pokémon
-
-    return pokemon
-}
-
-
-
-// Obtém detalhes de um Pokémon específico usando sua URL
-pokeApi.getPokemonDetail = (pokemon) => {
-    return fetch(pokemon.url)
-            .then((response) => response.json())
-            .then(convertPokeApiDetailToPokemon)
-}
-
-// Obtém uma lista de Pokémon da PokeAPI com base no deslocamento e limite
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
-    const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}` // Constrói a URL para a requisição da lista de Pokémon
-    
     // Faz uma requisição para obter a lista de Pokémon
     return fetch(url)
-         .then((response) => response.json())
-         .then((jsonBody) => jsonBody.results)
-         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail)) // Para cada Pokémon na lista, obtém detalhes usando getPokemonDetail
-         .then((detailRequests) => Promise.all(detailRequests)) // Aguarda todas as requisições de detalhes serem concluídas
-         .then((getPokemonsDetails) => getPokemonsDetails) // Retorna os detalhes dos Pokémon
+        .then(function(response) {
+            // Transforma a resposta em JSON
+            return response.json();
+        })
+        .then(function(jsonBody) {
+            // Acessa a propriedade 'results' do corpo JSON
+            var pokemons = jsonBody.results;
+
+            // Cria uma matriz para armazenar as promessas das requisições de detalhes
+            var detailPromises = [];
+
+            // Itera sobre cada Pokémon na lista
+            for (var i = 0; i < pokemons.length; i++) {
+                // Adiciona a promessa da requisição de detalhes à matriz
+                detailPromises.push(pokeApi.getPokemonDetail(pokemons[i]));
+            }
+
+            // Aguarda todas as requisições de detalhes serem concluídas
+            return Promise.all(detailPromises);
+        })
+        .then(function(getPokemonsDetails) {
+            // Retorna os detalhes dos Pokémon
+            return getPokemonsDetails;
+        });
+};
+
+// Função que obtém detalhes de um Pokémon específico usando sua URL
+pokeApi.getPokemonDetail = function(pokemon) {
+    // Faz uma requisição para obter os detalhes do Pokémon usando sua URL
+    return fetch(pokemon.url)
+        .then(function(response) {
+            // Transforma a resposta em JSON
+            return response.json();
+        })
+        .then(function(pokemonDetail) {
+            // Chama uma função para converter os detalhes da PokeAPI nos detalhes esperados de um Pokémon
+            return convertPokeApiDetailToPokemon(pokemonDetail);
+        });
+};
+
+// Função para converter detalhes da PokeAPI em objetos Pokemon
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    // Cria uma nova instância de Pokémon
+    const pokemon = new Pokemon();
+
+    // Atribui o número e o nome do Pokémon com base nos detalhes da PokeAPI
+    pokemon.number = pokeDetail.id;
+    pokemon.name = pokeDetail.name;
+
+    // Cria um array para armazenar os tipos do Pokémon
+    const types = [];
+
+    // Itera sobre os slots de tipo e adiciona cada tipo ao array
+    for (let i = 0; i < pokeDetail.types.length; i++) {
+        types.push(pokeDetail.types[i].type.name);
+    }
+
+    // Obtém o primeiro tipo (para casos em que um Pokémon tem mais de um tipo)
+    const [type] = types;
+
+    // Configura as propriedades do Pokémon
+    pokemon.types = types;
+    pokemon.type = type;
+
+    // Obtém a URL da imagem do Pokémon
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+
+    // Retorna o objeto Pokémon configurado
+    return pokemon;
 }
-
-
