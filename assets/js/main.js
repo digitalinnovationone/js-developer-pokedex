63f050674b336bdba7efdbd5d9cecdeb0ac6ae30
Javan Saldanha <javan.saldanha@gmail.com>
2023-12-21 20:27:42 -0300

    pokedetail javan

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,28 +7,58 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
+        <li class="pokemon ${pokemon.type}" id="pokemon-${pokemon.number}">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
             <div class="detail">
                 <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    ${pokemon.types.map(type => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
-
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
             </div>
         </li>
-    `
+    `;
 }
+
 
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML += newHtml
+        const newHtml = pokemons.map(convertPokemonToLi).join('');
+        pokemonList.innerHTML += newHtml;
+
+        pokemons.forEach((pokemon) => {
+            const pokemonElement = document.getElementById(`pokemon-${pokemon.number}`);
+            pokemonElement.addEventListener('click', () => {
+                openModal(pokemon);
+            });
+        });
     })
 }
+
+function openModal(pokemon) {
+    const modal = document.getElementById('pokemonModal');
+    const modalBody = document.getElementById('modalBody');
+
+    modal.style.display = 'block';
+
+    modalBody.innerHTML = convertPokemonToLi(pokemon)
+
+    // Fechar o modal
+    const closeButton = document.querySelector('.close');
+    closeButton.onclick = function() {
+        modal.style.display = 'none';
+    }
+
+    // Fechar ao clicar fora do modal
+    window.onclick = function(event) {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    }
+}
+
+
 
 loadPokemonItens(offset, limit)
 
