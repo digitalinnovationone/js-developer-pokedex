4133d825d0ddd99655dd44f31cdcbce783070ca5
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2024-01-03 15:15:01 -0300

    refactor: add constructor to pokemon entity

--- script/model/pokemon.js
+++ script/model/pokemon.js
@@ -1,8 +1,14 @@
-
 class Pokemon {
-    number;
-    name;
-    type;
-    types = [];
-    photo;
+  number;
+  name;
+  type;
+  types = [];
+  photo;
+  constructor(number, name, type, types, photo) {
+    this.number = number;
+    this.name = name;
+    this.type = type;
+    this.types = types;
+    this.photo = photo;
+  }
 }
