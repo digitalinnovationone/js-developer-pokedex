0ba7f396cccca626f7d89364c2ea9a8cb4f2a0d0
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2024-01-03 16:11:44 -0300

    refactor: move pokedex state logic to separate class

--- script/main.js
+++ script/main.js
@@ -1,30 +1,9 @@
 const pokemonList = document.getElementById("pokemonList");
 const loadMoreButton = document.getElementById("loadMoreButton");
 
-const MAX_RECORDS = 151;
-const LIMIT = 10;
-let offset = 0;
-
-async function loadPokemonCards(offset, limit) {
-  const pokemons = await PokeAPI.getPokemons(offset, limit);
-  renderPokemonList(pokemons, pokemonList);
-}
-function loadMorePokemon() {
-  offset += LIMIT;
-  const qtdRecordsWithNexPage = offset + LIMIT;
-
-  if (qtdRecordsWithNexPage >= MAX_RECORDS) {
-    const newLimit = MAX_RECORDS - offset;
-    loadPokemonCards(offset, newLimit);
-
-    loadMoreButton.parentElement.removeChild(loadMoreButton);
-  } else {
-    loadPokemonCards(offset, LIMIT);
-  }
-}
-
 async function main() {
-  await loadPokemonCards(offset, LIMIT);
-  loadMoreButton.addEventListener("click", loadMorePokemon);
+  const pokedex = new Pokedex(151, 10, pokemonList);
+  await pokedex.loadMorePokemon();
+  loadMoreButton.addEventListener("click", () => pokedex.loadMorePokemon());
 }
 main();
