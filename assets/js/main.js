bb8f53392aa63db5413732f11f575fd4c92311aa
ryanciusjmak <ryanciusjmak@gmail.com>
2024-01-11 00:06:39 -0300

    Alteração e adiçãode modificações na Poketunes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
+const loadMoreDetailsButton = document.getElementById('loadMoreDetailsButton')
 
 const maxRecords = 151
 const limit = 10
@@ -7,7 +8,7 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
+        <li id="${pokemon.number}" class="pokemon ${pokemon.type}" data-pokemon='${JSON.stringify(pokemon)}'>
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
@@ -16,9 +17,13 @@ function convertPokemonToLi(pokemon) {
                     ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
 
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
             </div>
+            <div class="pagination">
+                <button id="loadMoreDetailsButton" type="button">
+                    More Details
+                </button>
+        </div>
         </li>
     `
 }
@@ -45,3 +50,61 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+pokemonList.addEventListener('click', function (event) {
+    const clickedPokemon = event.target.closest('.pokemon');
+    if (clickedPokemon) {
+        const pokemonData = JSON.parse(clickedPokemon.getAttribute('data-pokemon'));
+        console.log(pokemonData);
+        const pokemonName = pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1);
+        const modal = new bootstrap.Modal(document.getElementById('pokemonModal'));
+        const modalBody = document.getElementById('pokemonModalBody');
+
+        modalBody.innerHTML = `
+            <div class="row pokemon ${pokemonData.type}">
+            
+                <div class="modal-header">
+                    <h2 class="modal-title">${pokemonData.name} - #${pokemonData.number}</h2>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <br>
+                <div class="poke-img-container">
+                    <img height="150" src="${pokemonData.photo}" alt="${pokemonData.name}">
+                </div>
+                <div class="moredetail">
+                    <table>
+                        <thead>
+                            <tr class="poke-about-headline">
+                                <th>About</th>
+                                <th>Base Stats</th>
+                                <th>Evolution</th>
+                                <th>Moves</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- About section -->
+                            <tr>
+                                <td>Species</td>
+                                <td colspan="3">${pokemonData.species}</td>
+                            </tr>
+                            <tr>
+                                <td>Height</td>
+                                <td colspan="3">${pokemonData.height}</td>
+                            </tr>
+                            <tr>
+                                <td>Weight</td>
+                                <td colspan="3">${pokemonData.weight}</td>
+                            </tr>
+                            <tr>
+                                <td>Abilities</td>
+                                <td colspan="3">${pokemonData.abilities}</td>
+                            </tr>   
+                        </tbody>
+                    </table>                        
+                </div>               
+            </div>
+        `;
+
+        modal.show();
+    }
+});
--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -1,8 +1,12 @@
-
 class Pokemon {
     number;
     name;
     type;
     types = [];
     photo;
+    stats = [];
+    abilities = [];
+    species = [];
+    height;
+    weight;
 }
