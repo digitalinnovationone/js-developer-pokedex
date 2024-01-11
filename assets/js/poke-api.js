444efcf8f55dff4bdd4da9a99754e223886f045d
lukeskw <lucasporfirioa@gmail.com>
2024-01-11 19:46:19 -0300

    feat/docs: creating pokemon details modal and writing docs

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -5,12 +5,26 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
     pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
+    pokemon.height = pokeDetail.height
+    pokemon.weight = pokeDetail.weight
 
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
 
     pokemon.types = types
     pokemon.type = type
+    const abilities = pokeDetail.abilities.map((abilitySlot) =>  abilitySlot.ability.name)
+    const [ability] = abilities
+
+    pokemon.abilities = abilities
+    pokemon.ability = ability
+
+    const baseStats = {};
+    pokeDetail.stats.forEach((statData) => {
+        baseStats[statData.stat.name] = statData.base_stat;
+    });
+
+    pokemon.stats = baseStats;
 
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
 
@@ -33,3 +47,11 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)
 }
+
+pokeApi.getPokemonDetailsById = (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon);
+};
--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -2,7 +2,12 @@
 class Pokemon {
     number;
     name;
+    height;
+    weight;
     type;
     types = [];
+    ability;
+    abilities = [];
+    stats = [];
     photo;
 }
