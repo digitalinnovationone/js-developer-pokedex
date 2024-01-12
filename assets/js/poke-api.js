68665886e1364ef6abaeb0da5d3f7041144a614e
Luiz Henrique Pinheiro Silva <luizhenriquepinheirosilva@hotmail.com>
2024-01-12 15:31:25 -0300

    page detail pokemon

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,35 +1,49 @@
+const pokeApi = {};
 
-const pokeApi = {}
+function convertPokeApiDetailToPokemon(pokeDetail, infoSpecies) {
+  const pokemon = new Pokemon();
+  pokemon.number = pokeDetail.id;
+  pokemon.name = pokeDetail.name;
+  pokemon.description = infoSpecies?.flavor_text_entries[0]?.flavor_text;
 
-function convertPokeApiDetailToPokemon(pokeDetail) {
-    const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.id
-    pokemon.name = pokeDetail.name
+  pokemon.stats.hp = pokeDetail.stats.find((item) => item.stat.name === 'hp');
+  pokemon.stats.atk = pokeDetail.stats.find((item) => item.stat.name === 'attack');
+  pokemon.stats.def = pokeDetail.stats.find((item) => item.stat.name === 'defense');
+  pokemon.stats.sAtk = pokeDetail.stats.find((item) => item.stat.name === 'special-attack');
+  pokemon.stats.sDef = pokeDetail.stats.find((item) => item.stat.name === 'special-defense');
+  pokemon.stats.spd = pokeDetail.stats.find((item) => item.stat.name === 'speed');
 
-    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
-    const [type] = types
+  const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+  const [type] = types;
 
-    pokemon.types = types
-    pokemon.type = type
+  pokemon.types = types;
+  pokemon.type = type;
 
-    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+  pokemon.photo = pokeDetail.sprites.other.showdown.front_default;
 
-    return pokemon
+  return pokemon;
 }
 
 pokeApi.getPokemonDetail = (pokemon) => {
-    return fetch(pokemon.url)
-        .then((response) => response.json())
-        .then(convertPokeApiDetailToPokemon)
-}
+  return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then(convertPokeApiDetailToPokemon);
+};
 
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+pokeApi.getPokemons = async (offset = 0, limit = 5) => {
+  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-    return fetch(url)
-        .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)
-        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-        .then((detailRequests) => Promise.all(detailRequests))
-        .then((pokemonsDetails) => pokemonsDetails)
-}
+  try {
+    const response = await fetch(url);
+    const jsonBody = await response.json();
+    const pokemons = jsonBody.results;
+
+    const detailRequests = pokemons.map((pokemon) => pokeApi.getPokemonDetail(pokemon));
+    const pokemonsDetails = await Promise.all(detailRequests);
+
+    return pokemonsDetails;
+  } catch (error) {
+    console.error('Erro ao obter pokémons:', error);
+    throw error;
+  }
+};
