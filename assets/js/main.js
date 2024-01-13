b5a306fe5f2615e1f045c0b84b6faa82c63a94ba
Andre S Nascimento <andresoaresnascimento@gmail.com>
2024-01-13 00:54:26 -0300

    Projeto final com modal  em CSS

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,26 +15,29 @@ function convertPokemonToLi(pokemon) {
                 <ol class="types">
                     ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
-
+                <a href="#card-${pokemon.number}">
                 <img src="${pokemon.photo}"
                      alt="${pokemon.name}">
+                </a>
             </div>
         </li>
-        <div id="card" class="cardStats ${pokemon.type}">
-        <h2>${pokemon.name}</h2>
-        <img src="${pokemon.photo}"  alt="${pokemon.name}">
-        <ul class="cardStats">
-            <li class="cardStats"><p><span class="title">Habilidade 1:</span><span class="value">${pokemon.abilities[0]}</span></p></li>
-            <li class="cardStats"><p><span class="title">Habilidade 2:</span><span class="value">${pokemon.abilities[1]}</span></p></li>
-            <li class="cardStats"><p class="info ${pokemon.type}">Info</p></li>
-            <li class="cardStats"><p><span class="title">HP:</span><span class="value">${pokemon.hp}</span></p></li>
-            <li class="cardStats"><p><span class="title">Ataque: </span><span class="value">${pokemon.attack}</span></p></li>
-            <li class="cardStats"><p><span class="title">Defesa: </span><span class="value">${pokemon.defense}</span></p></li>
-            <li class="cardStats"><p><span class="title">Ataque Especial: </span><span class="value">${pokemon.specialAttack}</span></p></li>
-            <li class="cardStats"><p><span class="title">Defesa Especial: </span><span class="value">${pokemon.specialDefense}</span></p></li>
-            <li class="cardStats"><p><span class="title">Valocidade: </span><span class="value">${pokemon.specialDefense}</span></p></li>
-        </ul>
-    </div>
+        
+        <div id="card-${pokemon.number}" class="modal-window cardStats ${pokemon.type}">
+            <h2>${pokemon.name}</h2><a href="#" title="Close" class="modal-close">⨉</a>
+            <img src="${pokemon.photo}"  alt="${pokemon.name}">
+            <ul class="cardStats">
+                <li class="cardStats"><p><span class="title">Habilidade 1:</span><span class="value">${pokemon.abilities[0]}</span></p></li>
+                <li class="cardStats"><p><span class="title">Habilidade 2:</span><span class="value">${pokemon.abilities[1]}</span></p></li>
+                <li class="cardStats"><p class="info ${pokemon.type}">Info</p></li>
+                <li class="cardStats"><p><span class="title">HP:</span><span class="value">${pokemon.hp}</span></p></li>
+                <li class="cardStats"><p><span class="title">Ataque: </span><span class="value">${pokemon.attack}</span></p></li>
+                <li class="cardStats"><p><span class="title">Defesa: </span><span class="value">${pokemon.defense}</span></p></li>
+                <li class="cardStats"><p><span class="title">Ataque Especial: </span><span class="value">${pokemon.specialAttack}</span></p></li>
+                <li class="cardStats"><p><span class="title">Defesa Especial: </span><span class="value">${pokemon.specialDefense}</span></p></li>
+                <li class="cardStats"><p><span class="title">Valocidade: </span><span class="value">${pokemon.specialDefense}</span></p></li>
+            </ul>
+        </div>
+        
     `
 }
 
@@ -42,6 +45,7 @@ function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
         const newHtml = pokemons.map(convertPokemonToLi).join('')
         pokemonList.innerHTML += newHtml
+        
     })
 }
 
@@ -60,3 +64,4 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
