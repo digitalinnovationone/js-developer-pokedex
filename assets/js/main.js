02da05689340133be2717c8b4b11c846839d92fb
NatanaelJose <josesaraivamaciel@gmail.com>
2024-01-22 20:42:27 -0300

    feat: search bar and css upgrades

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
+const searchButton = document.getElementById('search-btn')
+const searchInput = document.getElementById('search')
 
 const maxRecords = 151
 const limit = 10
@@ -31,6 +33,27 @@ function loadPokemonItens(offset, limit) {
 }
 
 loadPokemonItens(offset, limit)
+
+function performSearch() {
+    pokemonList.innerHTML = '';
+    pokeApi.getAll().then((allPokemonList) => {
+        const searchTerm = searchInput.value.trim().toLowerCase();
+        const searchResults = pokeApi.search(searchTerm, allPokemonList);
+        const newHtml = searchResults.map(convertPokemonToLi).join('');
+        pokemonList.innerHTML = newHtml;
+    });
+}
+
+searchButton.addEventListener('click', () => {
+    performSearch();
+});
+
+searchInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        performSearch();
+    }
+});
 
 loadMoreButton.addEventListener('click', () => {
     offset += limit
