30c02fffbc626a17ff409abd4816d3ecdfb58b00
erick albuquerque <ericgtxh@Hotmail.com>
2024-01-27 19:26:07 -0300

    pequena mudanca

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 
 const maxRecords = 151
-const limit = 10
+const limit = 12
 let offset = 0;
 
 function convertPokemonToLi(pokemon) {
