213710135e762f027970aa002df67da39fba0a6f
Deigela_Lima <deigelalima@hotmail.com>
2024-01-29 11:25:34 -0300

    Adicionando os detalhes do Pokémon.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Trilha JS Developer - Pokedex
+
+Esse projeto da Pokédex foi desenvolvido durante o Bootcamp da [Digital Innovation One - DIO]([URL](https://web.dio.me/home)) sobre Desenvolvimento Frontend com Angular. Trata-se de uma aplicação que utiliza a [PokeApi](https://pokeapi.co/) para apresentar detalhes sobre os Pokémon, incluindo suas habilidades. O desenvolvimento da aplicação foi utilizando JavaScript, HTML e CSS. A Pokédex proporciona aos usuários a oportunidade de selecionar e explorar informações sobre um Pokémon específico. Esse projeto teve como inspiração o projeto da [Camila Correia](https://github.com/Camesis/js-developer-pokedex), onde foi compreendido e aplicando conforme desejado.
+
+### * Homepage
+![Home](/assets/img/pokemons-home.png)
+
+### * Detalhe do Pokémon
+![Detalhes do Pokémon](/assets/img/details-pokemon.png)
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,22 +3,23 @@ const loadMoreButton = document.getElementById('loadMoreButton')
 
 const maxRecords = 151
 const limit = 10
-let offset = 0;
+let offset = 0
 
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
+            <span class="numPokemon">#${pokemon.numPokemon}</span>
             <span class="name">${pokemon.name}</span>
+            <a href="details-pokemon.html?id=${pokemon.numPokemon}">
 
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
 
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
-            </div>
+                    <img src="${pokemon.photo}" alt="${pokemon.name}">
+                </div>
+            </a>
         </li>
     `
 }
