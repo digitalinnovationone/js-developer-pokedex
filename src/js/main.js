b4b585cb9055b29a7980d43d7bb604ca0f3485e8
PlopesK <popi081004@hotmail.com>
2024-01-30 23:26:05 -0300

    Organizations change + desing

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,5 @@
 const pokemonList = document.getElementById("pokemonList");
 const loadMoreButton = document.getElementById("loadMoreButton");
-const overlay = document.getElementById("overlay");
-const pokemonDetail = document.getElementById("pokemonDetail");
 
 const maxRecords = 151;
 const limit = 12;
@@ -51,73 +49,3 @@ loadMoreButton.addEventListener("click", () => {
     loadPokemonItens(offset, limit);
   }
 });
-
-function PokemonByID(pokemon) {
-  return `
-        <div class="modal ${pokemon.type}">
-            <span class="close" onclick="closePokemonDetail()">&times;</span>
-            <span class="name">${pokemon.name}</span>
-            <span class="number">#${pokemon.number}</span>
-            <div class="info">
-                <img src="${pokemon.photo}"
-                alt="${pokemon.name}">
-
-                <div class="detail">
-                <ol class="types row">
-                    ${pokemon.types
-                      .map((type) => `<li class="type ${type}">${type}</li>`)
-                      .join("")}
-                </ol>
-                </div>
-                
-                <div class="about">
-                    <div class="title"> <hr> Sobre <hr> </div>
-                    <p>Altura: ${pokemon.altura}</p>
-                    <p>Peso: ${pokemon.peso}</p>
-
-                    <ul class="stats-container">
-                        <div class="title"> <hr> Status <hr> </div>
-                        ${Object.entries(pokemon.stats)
-                          .map(
-                            ([statName]) =>
-                              `<li class='stats'>
-                            <p>${statName}</p>
-                            <p>${pokemon.stats[statName].base_stat}</p>
-                            <span class='progress-bar dark'>
-                            <span style="width: ${
-                              pokemon.stats[statName].base_stat > 100
-                                ? 100
-                                : pokemon.stats[statName].base_stat
-                            }%" 
-                                class='progress ${pokemon.type}'/></span>
-                        </li>`
-                          )
-                          .join("")}
-                  </ul>
-                </div>
-            </div>
-        </div>
-    `;
-}
-
-function showPokemonDetail(pokemonId) {
-  pokeApi.getPokemonByID(pokemonId).then((data) => {
-    const singlePokemon = PokemonByID(data);
-    Modal(singlePokemon);
-  });
-  overlay.addEventListener("click", () => {
-    overlay.style.display = "none";
-    pokemonDetail.style.display = "none";
-  });
-}
-
-function Modal(singlePokemon) {
-  overlay.style.display = "block";
-  pokemonDetail.style.display = "block";
-  pokemonDetail.innerHTML = singlePokemon;
-}
-
-function closePokemonDetail() {
-  overlay.style.display = "none";
-  pokemonDetail.style.display = "none";
-}
