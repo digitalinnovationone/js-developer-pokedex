78efa361d9e67729e2a319c84cbba2f1e272b438
alexsanderthorne <alexsanderthorne@gmail.com>
2024-02-02 16:47:12 -0300

    implementando overlay ao abrir card

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,17 +34,47 @@ function loadPokemonItens(offset, limit) {
 loadPokemonItens(offset, limit)
 
 function expandPokemon(element) {
+    // Remove a overlay existente
+    const existingOverlay = document.querySelector('.overlay');
+    if (existingOverlay) {
+        existingOverlay.remove();
+    }
+
+    // Cria a overlay
     const overlay = document.createElement('div');
     overlay.classList.add('overlay');
+    overlay.addEventListener('click', closeOverlay); // Adiciona um event listener para fechar ao clicar na overlay
     document.body.appendChild(overlay);
 
+    // Clona o item e adiciona a classe 'expanded'
     const expandedPokemon = element.cloneNode(true);
     expandedPokemon.classList.add('expanded');
+
+    // Adiciona o botão de fechar ao item expandido
+    const closeButton = document.createElement('button');
+    closeButton.classList.add('close-btn');
+    closeButton.textContent = 'X';
+    closeButton.addEventListener('click', closeOverlay);
+    expandedPokemon.appendChild(closeButton);
+
+    // Adiciona o item expandido ao corpo do documento
     document.body.appendChild(expandedPokemon);
 
     // Adiciona classe de destaque ao item clicado
     element.classList.add('highlight');
-} 
+}
+
+// Função para fechar o item expandido e a overlay
+function closeOverlay() {
+    const overlay = document.querySelector('.overlay');
+    overlay.remove();
+
+    const expandedPokemon = document.querySelector('.pokemon.expanded');
+    expandedPokemon.remove();
+}
+
+
+
 
 loadMoreButton.addEventListener('click', () => {
     // Mostra o spinner
